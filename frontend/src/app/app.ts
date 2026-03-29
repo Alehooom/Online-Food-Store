7ac363bc8ec8f1c3d78631ff0a309d5658af1c8c
Alehooom <ellafhassan7@gmail.com>
2026-03-29 21:35:22 +0200

    Part 4

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// Kontrollera att det står 'partials' här nu!
+import { Header } from './components/partials/header/header'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, Header], // Header måste ligga här
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('fronteend');
+  protected readonly title = signal('frontend'); 
 }
